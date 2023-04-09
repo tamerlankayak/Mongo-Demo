8f7966fd813168a2c6720833c7eef5a1db5b8bde
Tamerlan Mustafayev <tamerlan_94@mail.ru>
2023-04-09 23:30:23 +0400

    Update ProductRepository.java

--- src/main/java/az/tamerlan/mongodemo/product/ProductRepository.java
+++ src/main/java/az/tamerlan/mongodemo/product/ProductRepository.java
@@ -1,7 +1,7 @@
 package az.tamerlan.mongodemo.product;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
+//extendinng to MongoRepository for CRUD operations
 public interface ProductRepository extends MongoRepository<Product, String> {
 
 }
