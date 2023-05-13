9f17041d82c3532c741fd99be234a05e402b168a
Tamerlan Mustafayev <tamerlan_94@mail.ru>
2023-05-14 00:12:46 +0400

    Update CategoryRepository.java

--- src/main/java/az/tamerlan/mongodemo/category/CategoryRepository.java
+++ src/main/java/az/tamerlan/mongodemo/category/CategoryRepository.java
@@ -1,6 +1,6 @@
 package az.tamerlan.mongodemo.category;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-
+//extending from MongoRepository class for inhertince and functionallyty of mongo
 public interface CategoryRepository extends MongoRepository<Category,String> {
 }
